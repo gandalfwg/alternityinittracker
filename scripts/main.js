50ddff5e60dcd5df813d68ed66f5019688cb7a2d
Anthony <gandalfwg@gmail.com>
2020-04-21 17:38:20 -0500

    Adding real code

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,52 @@ Hooks.on("init", function() {
 Hooks.on("ready", function() {
   console.log("This code runs once core initialization is ready and game data is available.");
 });
+
+var combatVar;
+if(game.user.isGM) {
+
+Hooks.on("createCombatant", function(){
+  console.log("createCombatant called.");
+  combatVar = this;
+});
+
+function setNewCombatantInit() {
+  var newCombatant = combatVar.combantants[combatVar.combantants.length - 1];
+}
+
+Hooks.on("nextTurn", function(){
+  console.log("nextTurn called.");
+  combatVar = this;
+  createNextTurnDialog();
+});
+
+function createNextTurnDialog(){
+  var diagDiv = document.createElement("div");
+  diagDiv.id = "altinitdialog";
+  diagDiv.classList += "app";
+  var spanVar = document.createElement("span");
+  spanVar.innerHTML = "How many Impulses to add?";
+  diagDiv.appendChild(spanVar);
+  var inputVar = document.createElement("input");
+  inputVar.id = "altinitdiagimpulse";
+  inputVar.type = "text";
+  diagDiv.appendChild(inputVar);
+  var buttonVar = document.createElement("button");
+  buttonVar.type = "button";
+  buttonVar.innerHTML = "Add";
+  buttonVar.addEventListener(addImpulses);
+  diagDiv.appendChild(buttonVar);
+  document.body.appendChild(diagDiv);
+}
+
+function addImpulses(){
+  var impulseInput = document.getElementById("altinitdiagimpulse").value;
+  console.log("Impulses to add: " + impulseInput.value);
+  document.getElementById("altinitdialog").remove();
+}
+
+function setPrevActorInit(){
+
+}
+
+}
